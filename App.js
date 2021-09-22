61dff0171b5a056be0fd02b16d09d5086866130e
tanavsharma <sharma.tanav95@gmail.com>
2021-09-22 19:23:28 -0400

    Created the labels, inputs, & submit button

--- App.js
+++ App.js
@@ -18,11 +18,30 @@ import {
 
 class Inputs extends Component{
 
+  
+
   render() {
     return (
       <View style = {styles.container}>
 
       <Text style = {styles.title}>BMI Calculator</Text>
+
+      <Text style = {styles.label}>Height</Text>
+      <TextInput style = {styles.input}/>
+
+      <Text style = {styles.label}>Weight</Text>
+      <TextInput style = {styles.input}/>
+
+      <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.calculate(this.state.height, this.state.weight)
+               }>
+               <Text style = {styles.submitButtonText}> Calculate </Text>
+      </TouchableOpacity>
+
+      <Text style = {styles.label}>Your BMI</Text>
+      
 
       </View>
     )
@@ -43,5 +62,28 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     fontWeight: "bold",
+  },
+  label: {
+    marginLeft: 15,
+  },
+  input: {
+    margin:15,
+    height: 40,
+    borderWidth: 1,
+    padding:10,
+  },
+  submitButton:{
+    padding: 10,
+    margin: 15,
+    height: 40,
+    backgroundColor: "#FF6677"
+  },
+  submitButtonText:{
+    textAlign: "center",
+    color:'white',
+    fontSize: 18,
   }
+
+
+
 })
